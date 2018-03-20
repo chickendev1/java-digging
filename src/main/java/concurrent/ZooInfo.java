3a6e71be06bc21cff0178fb491fb2c6a05ac47b7
Nguyen Quang Thu <thunq@Nguyens-MBP.lan>
2018-03-20 14:42:33 +0700

    add concurrent package

---
+++ src/main/java/concurrent/ZooInfo.java
@@ -0,0 +1,23 @@
+package concurrent;
+
+import java.util.concurrent.*;
+
+public class ZooInfo {
+	public static void main(String[] args) {
+		ExecutorService service = null;
+		try {
+			service = Executors.newSingleThreadExecutor();
+			System.out.println("begin");
+			service.execute(() -> System.out.println("Printing zoo inventory"));
+			service.execute(() -> {
+				for (int i = 0; i < 10; i++)
+					System.out.println("Printing record: " + i);
+			});
+			service.execute(() -> System.out.println("Printing zoo inventory"));
+			System.out.println("end");
+		} finally {
+			if (service != null)
+				service.shutdown();
+		}
+	}
+}
